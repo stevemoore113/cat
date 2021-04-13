bcb253716b8ab8ac06d1f96c337145e42736bb52
stevehy_shih <stevehy_shih@compal.com>
2021-04-13 16:41:05 +0800

    add cs cs gene predict

--- cat/backend/route/bengal-route.js
+++ cat/backend/route/bengal-route.js
@@ -16,8 +16,50 @@ exports.bengalRouter = void 0;
 const koa_1 = __importDefault(require("koa"));
 const koa_router_1 = __importDefault(require("koa-router"));
 const app = new koa_1.default();
+function computeGene(papa, mama) {
+    let dimensions = [];
+    dimensions.push(papa);
+    dimensions.push(mama);
+    console.log(mama, papa);
+    let result = [];
+    function explore(curDim, prefix) {
+        var nextDim = dimensions.shift();
+        for (var i = 0; i < curDim.length; i++) {
+            if (nextDim)
+                explore(nextDim, prefix + curDim[i] + "/");
+            else
+                result.push(prefix + curDim[i]);
+        }
+        if (nextDim)
+            dimensions.push(nextDim);
+    }
+    explore(dimensions.shift(), "");
+    return result;
+}
+class BengalConstroller {
+    static compute(ctx, next) {
+        return __awaiter(this, void 0, void 0, function* () {
+            /**
+             * []
+             * 獨立 雪豹snow ['cs','x'] ['cb','x'] '['cs','cs']' '['cb','cs']' '['cb','cb']'  ['x','x']
+             * 獨立 銀豹sliver ['s','x'] ['s','x']
+             * 合一 炭豹charcoa ['abp','a'] ['abp','abp'] ['abp','A']
+             * 合一 黑豹black ['a','a'] ['A','a']
+             * 獨立 閃光flaten ['b','x']['x','x']
+             */
+            const gene = { snow: ['', ''], snow2: ['', ''] };
+            gene.snow = ['cs', 'cs'];
+            gene.snow2 = ['x', 'cs'];
+            const resSnow = computeGene(gene.snow, gene.snow2);
+            console.log(resSnow);
+            // const resCharcoa = computeGene(gene.Char, gene.Char2);
+            // const resSliver = computeGene(gene.sliver, gene.sliver2);
+            // const resFlat = computeGene(gene.flat, gene.flat2);
+            // const resBlack = computeGene(gene.black, gene.black2);
+            yield next();
+        });
+    }
+}
 const bengalRouter = new koa_router_1.default();
 exports.bengalRouter = bengalRouter;
-bengalRouter.get('/', (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
-    ctx.body = 'index';
-}));
+bengalRouter.get('/', BengalConstroller.compute);
--- cat/backend/route/bengal-route.ts
+++ cat/backend/route/bengal-route.ts
@@ -1,10 +1,57 @@
+import { getKernel } from '@tensorflow/tfjs-core';
 import Koa from 'koa';
 import Router from 'koa-router';
 
 const app = new Koa();
 
+function computeGene(papa: any[], mama: any[]) {
+  let dimensions: any[] = [];
+  dimensions.push(papa);
+  dimensions.push(mama);
+
+  console.log(mama, papa);
+
+  let result: string[] = [];
+  function explore(curDim: string | any[], prefix: string) {
+    var nextDim = dimensions.shift();
+    for (var i = 0; i < curDim.length; i++) {
+      if (nextDim)
+        explore(nextDim, prefix + curDim[i] + "/");
+      else
+        result.push(prefix + curDim[i]);
+    }
+    if (nextDim) dimensions.push(nextDim);
+  }
+  explore(dimensions.shift(), "");
+  return result;
+}
+
+class BengalConstroller {
+  static async compute(ctx: any, next: Function) {
+    /**
+     * []
+     * 獨立 雪豹snow ['cs','x'] ['cb','x'] '['cs','cs']' '['cb','cs']' '['cb','cb']'  ['x','x']
+     * 獨立 銀豹sliver ['s','x'] ['s','x'] 
+     * 合一 炭豹charcoa ['abp','a'] ['abp','abp'] ['abp','A']
+     * 合一 黑豹black ['a','a'] ['A','a']
+     * 獨立 閃光flaten ['b','x']['x','x']
+     */
+
+    const gene = { snow: ['', ''], snow2: ['', ''] };
+    gene.snow = ['cs', 'cs'];
+    gene.snow2 = ['x', 'cs'];
+    const resSnow = computeGene(gene.snow, gene.snow2);
+
+    console.log(resSnow);
+    // const resCharcoa = computeGene(gene.Char, gene.Char2);
+    // const resSliver = computeGene(gene.sliver, gene.sliver2);
+    // const resFlat = computeGene(gene.flat, gene.flat2);
+    // const resBlack = computeGene(gene.black, gene.black2);
+
+    await next();
+  }
+}
+
 const bengalRouter = new Router();
-bengalRouter.get('/', async (ctx: { body: string; }, next) => {
-  ctx.body = 'index';
-});
+bengalRouter.get('/', BengalConstroller.compute);
 export { bengalRouter };
