f16eb1d4bc30650bfd426e7fb48e4b89fa358d8c
stevehy_shih <stevehy_shih@compal.com>
2021-04-16 09:36:33 +0800

    rebuid frontend

--- cat/frontend/app/main.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-function App() {
-  return <div>Hello React < /div>
-}
-
-render(<App />, document.getElementById('app'));
--- cat/frontend/build/utils.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const path = require('path');
-exports.resolve = (...arg) => path.join(__dirname, '..', ...arg);
